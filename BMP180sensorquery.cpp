c5f2d1e3e43362f33d0c86eb4b5896470fc7837a
Moisi Xhaferaj <elektroxray@gmail.com>
2018-12-05 21:05:39 +0100

    Update the licensing

--- BMP180sensorquery.cpp
+++ BMP180sensorquery.cpp
@@ -1,12 +1,26 @@
 /*
 
+Copyright (C) 2018  Moisi Xhaferaj
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 BMP180sensorquery Library for BMP-180 querying functions 
 
 These functions have the purpose of reading through the BMP-180 library the output of the sensor
 connected through the I2C bus to the Arduino Uno master.
 
 Created by Moisi Xhaferaj, 05/03/2018.
-Released into the public domain.
 
 Original code from:
 
