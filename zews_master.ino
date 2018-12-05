878eda802d0083f45e5f67be2adc65174b92bad0
Moisi Xhaferaj <elektroxray@gmail.com>
2018-12-05 21:03:00 +0100

    Update to teh licensing

--- zews_master.ino
+++ zews_master.ino
@@ -1,4 +1,18 @@
 /*
+Copyright (C) 2018  Moisi Xhaferaj
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Code for the OSWS Arduino UNO Master 
 
@@ -6,7 +20,6 @@ These function calls have the purpose of reading the output of the sensors
 connected through the I2C bus or direvtly to the Arduino Uno master.
 
 Created by Moisi Xhaferaj, 05/03/2018.
-Released into the public domain.
 
 Part of the original code from:
 SFE_BMP180 library example sketch
